28987a1741d7f44d4c93ac9d087a3c58a27f15dc
Sai Abhi Ram <117653910+saiabhirampothini@users.noreply.github.com>
2024-03-27 18:34:37 +0530

    Update ProtectedRouteStudent.js

--- src/components/ProtectedRouteStudent.js
+++ src/components/ProtectedRouteStudent.js
@@ -2,13 +2,52 @@ import { Navigate } from "react-router-dom";
 import axios from "axios";
 const ProtectedRouteStudent = ({ children }) => {
   // console.log(isLoggedIn);
+  // if (!localStorage.token) {
+  //   return <Navigate to="/login" replace />;
+  // } else if (
+  //   localStorage.token &&
+  //   localStorage.getItem("profession") === "Student"
+  // )
+  //   return children;
+  // else return <Navigate to="/login" replace />;
+  const fetchUser = async () => {
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+        "x-auth-token": localStorage.getItem("token"),
+      };
+      let res = await axios.get(
+        "https://akova-backend.vercel.app/api/auth/protectRoute",
+        {
+          headers: headers,
+        }
+      );
+      // console.log(res);
+      if (res) {
+        if (res.data.profession !== localStorage.getItem("profession")) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("profession");
+          window.location.reload();
+          return <Navigate to="/login" replace />;
+        } else {
+          return children;
+        }
+      }
+    } catch (err) {
+      window.alert(err.data.msg);
+      localStorage.removeItem("token");
+      localStorage.removeItem("profession");
+      window.location.reload();
+      return <Navigate to="/login" replace />;
+    }
+  };
   if (!localStorage.token) {
     return <Navigate to="/login" replace />;
   } else if (
     localStorage.token &&
     localStorage.getItem("profession") === "Student"
-  )
-    return children;
-  else return <Navigate to="/login" replace />;
+  ) {
+    fetchUser();
+  } else return <Navigate to="/login" replace />;
 };
 export default ProtectedRouteStudent;
