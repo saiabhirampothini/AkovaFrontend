d92b56e7ecfeb88f22720e2d1827f016132aff4a
Sai Abhi Ram <117653910+saiabhirampothini@users.noreply.github.com>
2024-03-27 18:30:09 +0530

    Update ProtectedRouteInvestor.js

--- src/components/ProtectedRouteInvestor.js
+++ src/components/ProtectedRouteInvestor.js
@@ -1,4 +1,5 @@
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 const ProtectedRouteInvestor = ({ children }) => {
   // console.log(isLoggedIn);
 //   if (!localStorage.token) {
